97ea0588660f54b7849d707456cbc3d3b469e61d
Azim <azim210215@gmail.com>
2023-11-27 04:48:40 +0600

    Phone Details with Api and Modal Added

--- js/phone.js
+++ js/phone.js
@@ -36,7 +36,7 @@ const displayAllPhone = (allPhone, isShowAll) => {
             <h2 class="card-title">${phone.brand}</h2>
             <p>${phone.phone_name}</p>
             <div class="card-actions justify-end">
-              <button class="btn btn-accent">Show Details</button>
+              <button onclick='handleShowDetail("${phone.slug}")' class="btn btn-accent">Show Details</button>
             </div>
           </div>
         </div>
@@ -66,4 +66,48 @@ const isLoading = (loadingTerm) => {
 
 const handleShowAll = () => {
   handlePhoneSearch(true);
+};
+
+const handleShowDetail = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetails(phone);
+};
+
+const showPhoneDetails = (phone) => {
+  const modalContainer = document.getElementById("modalContainer");
+  modalContainer.textContent = "";
+  const modal = document.createElement("div");
+  modal.innerHTML = `
+    <div class="modal-box">
+           <div class="card bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="${phone.image}"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Brand: ${phone.brand}</h2>
+            <p>Model: ${phone.name}</p>
+            <p>ChipSet: ${phone.mainFeatures.chipSet}</p>
+            <p>Storage: ${phone.mainFeatures.storage}</p>
+            <p>Ram: ${phone.mainFeatures.memory}</p>
+            <p>Release Date: ${phone.releaseDate}</p>
+            
+          </div>
+        </div>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn">Close</button>
+              </form>
+            </div>
+          </div>
+    `;
+  modalContainer.appendChild(modal);
+  my_modal_5.showModal();
 };
